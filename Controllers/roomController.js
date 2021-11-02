3b2ef12bf7af074299ef71c685732cb335e3b045
AshikShuvo <ashikshuvo1996@gmail.com>
2021-11-02 12:29:33 +0600

    delete a single room

--- Controllers/roomController.js
+++ Controllers/roomController.js
@@ -47,8 +47,45 @@ const getSingleRoom=async (req,res)=>{
         res.status(400).json({error:error.message,success:false})
     }
 }
+
+const updateSingleRoom=async (req,res)=>{
+    try{
+        let room = await Room.findById(req.query.id);
+        if(!room){
+           return res.status(404).json({error:'room not found with this id',success:false})
+        }
+        room=await Room.findByIdAndUpdate(req.query.id,req.body,{
+            new:true,
+            runValidators:true,
+            useFindAndModify:false
+        })
+        res.status(200).json({
+            success:true,
+            room
+        })
+    }catch(error){
+        res.status(400).json({error:error.message,success:false})
+    }
+}
+const deleteSingleRoom=async (req,res)=>{
+    try{
+        const room = await Room.findById(req.query.id);
+        if(!room){
+           return res.status(404).json({error:'room not found with this id',success:false})
+        }
+        await room.remove();
+        res.status(200).json({
+            success:true,
+            message:"room is deleted"
+        })
+    }catch(error){
+        res.status(400).json({error:error.message,success:false})
+    }
+}
 export {
     allRooms,
     createNewRoom,
-    getSingleRoom
+    getSingleRoom,
+    updateSingleRoom,
+    deleteSingleRoom
 }
--- pages/api/rooms/[id].js
+++ pages/api/rooms/[id].js
@@ -1,9 +1,11 @@
 import nc from 'next-connect';
 import dbConnect from '../../../config/dbConnect';
-import { allRooms, createNewRoom, getSingleRoom } from '../../../Controllers/roomController';
+import {  getSingleRoom,updateSingleRoom,deleteSingleRoom } from '../../../Controllers/roomController';
 const handler=nc();
 
 dbConnect();
 
 handler.get(getSingleRoom);
+handler.put(updateSingleRoom)
+handler.delete(deleteSingleRoom)
 export default handler;
