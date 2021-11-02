d4ffa36630f1d7cddbbf74e12a6b5fa893e2651c
AshikShuvo <ashikshuvo1996@gmail.com>
2021-11-02 11:05:58 +0600

    get all room

--- Controllers/roomController.js
+++ Controllers/roomController.js
@@ -1,14 +1,19 @@
 import Room from "../modals/room"
 
-const allRooms=(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:"all rooms"
-    })
+const allRooms=async (req,res)=>{
+    try{
+        const rooms = await Room.find();
+        res.status(200).json({
+            success:true,
+            rooms
+        })
+    }catch(error){
+        res.status(400).json({error:error.message,success:false})
+    }
+    
 }
 
 const createNewRoom=async (req,res)=>{
-    console.log(req.body,"{}")
 try { 
    
     const room=await Room.create(req.body);
